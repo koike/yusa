a31432a534a80cacb0d3432b0af091c0b8eb1400
RintaroKOIKE <koike_rintaro@applibot.co.jp>
2017-02-16 16:25:52 +0900

    :new: First Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# yusa
+```yusa``` is support tool for [tomori](https://github.com/koike/tomori) and [ayumi](https://github.com/koike/ayumi)
+
+## Require
+- PHP 7
+- Composer
+
+## Install
+```sh
+# clone repository
+$ git clone https://github.com/koike/yusa.git
+
+# setup files
+$ composer install
+
+# write api key
+$ vi .env
+
+# register cron
+crontab cron.conf
+```
+
+## LICENSE
+```yusa``` is open-sourced software licensed under the [MIT license](LICENSE)
---
+++ yusa.php
@@ -0,0 +1,155 @@
+<?php
+
+require_once 'vendor/autoload.php';
+use GuzzleHttp\Client;
+
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
+class Request
+{
+    public static function get(string $url, $ua = null, $ref = null) : array
+    {
+        if(!is_string($url) || strlen($url) == 0)
+        {
+            return
+            [
+                'status'    =>  400,
+                'type'      =>  null,
+                'body'      =>  null
+            ];
+        }
+
+        $ua = 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; .NET CLR 3.0.04506.648)';
+        $ref = $url;
+
+        $client = new Client(['verify' => false]);
+        try
+        {
+            $response = $client->request
+            (
+                'GET',
+                $url,
+                [
+                    'headers'   =>
+                    [
+                        'User-Agent'    =>  $ua,
+                        'Referer'       =>  $ref
+                    ],
+                    'timeout'   =>  5
+                ]
+            );
+
+            return
+            [
+                'status'    =>  $response->getStatusCode(),
+                'type'      =>  $response->getHeader('Content-Type'),
+                'body'      =>  $response->getBody()
+            ];
+        }
+        catch(\Exception $e)
+        {
+            return
+            [
+                'status'    =>  500,
+                'type'      =>  null,
+                'body'      =>  null
+            ];
+        }
+    }
+}
+
+class Command
+{
+    public static function update()
+    {
+        Command::stop();
+        exec('cd ayumi && git pull', $out, $ret);
+        exec('cd tomori && git pull', $out, $ret);
+        Command::start();
+    }
+
+    public static function stop()
+    {
+        exec('ps aux | grep php', $out, $ret);
+        if(count($out) > 0)
+        {
+            $ps = [];
+            foreach($out as $line)
+            {
+                if(strpos($line, 'tomori') !== false || strpos($line, 'ayumi') !== false)
+                {
+                    $ps[] = $line;
+                }
+            }
+            if(count($ps) > 0)
+            {
+                foreach($ps as $p)
+                {
+                    $i = explode(' ', preg_replace('/[\s]{2,}/', ' ', $p));
+                    $pid = $i[1];
+                    exec('kill ' . $pid, $out, $ret);
+                }
+            }
+        }
+    }
+
+    public static function start()
+    {
+        exec('cd ayumi && nohup php tomori.php > /dev/null 2>&1 &', $out, $ret);
+        exec('cd ayumi && nohup php ayumi.php > /dev/null 2>&1 &', $out, $ret);
+    }
+}
+
+$url = getenv('URL');
+if(strlen($url) < 1)
+{
+    exit(-1);
+}
+$response = Request::get($url);
+$order = null;
+if($response['status'])
+{
+    $html = $response['body'];
+    $html = str_replace("\r", '', $html);
+    $html = explode("\n", $html);
+    $tweet = [];
+    foreach($html as $line)
+    {
+        if(strpos($line, '<div class="dir-ltr" dir="ltr">') !== false)
+        {
+            $tweet[] = trim(str_replace('<div class="dir-ltr" dir="ltr">', '', $line));
+        }
+    }
+    if(count($tweet) >= 2)
+    {
+        $order = $tweet[1];
+    }
+}
+if($order != null)
+{
+    if(file_exists('order.log'))
+    {
+        $old = file_get_contents('order.log');
+        if($order === $old)
+        {
+            exit(1);
+        }
+    }
+    
+    $command = explode(' ', $order);
+    switch($command[0])
+    {
+        case 'update':
+            Command::update();
+            break;
+        case 'stop':
+            Command::stop();
+            break;
+        case 'start':
+            Command::start();
+            break;
+    }
+
+    file_put_contents('order.log', $order);
+}
