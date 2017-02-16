d6234ae88177fb04279b1634d042eb8d6a240f0e
RintaroKOIKE <koike_rintaro@applibot.co.jp>
2017-02-16 18:04:56 +0900

    :up: Updated install.php

--- install.php
+++ install.php
@@ -1,7 +1,7 @@
 <?php
 
 exec('composer install', $out, $ret);
-exec('echo "*/10 * * * * php ' . getcwd() . '/yusa.php" > cron.conf', $out, $ret);
+exec('echo "*/5 * * * * php ' . getcwd() . '/yusa.php" > cron.conf', $out, $ret);
 exec('crontab cron.conf', $out, $ret);
 exec('echo "DIR=' . getcwd() . '/" >> .env', $out, $ret);
 echo PHP_EOL. 'Install Success!' . PHP_EOL;
