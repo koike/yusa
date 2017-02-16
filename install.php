6db24141fbecdfab722131bb9ae0df2c8aa08d19
RintaroKOIKE <koike_rintaro@applibot.co.jp>
2017-02-16 17:11:23 +0900

    :up: Updated install.php

--- install.php
+++ install.php
@@ -2,6 +2,6 @@
 
 $composer = 'composer install';
 exec($composer, $arr, $ret);
-exec('echo */10 * * * * php ' . getcwd() . '/yusa.php > cron.conf', $out, $ret);
+exec('echo "*/10 * * * * php ' . getcwd() . '/yusa.php" > cron.conf', $out, $ret);
 exec('crontab cron.conf', $out, $ret);
 echo PHP_EOL. 'Install Success!' . PHP_EOL;
